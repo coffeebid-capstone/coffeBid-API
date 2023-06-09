403456d18af1ffcadea90a3841070d6ad9a6dbb5
mansao01 <novrilukman15@gmail.com>
2023-06-09 20:07:19 +0700

    create endpoint for prodict coffee quality

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello():
     return "hello guys"
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predictModelRoast", methods=['POST'])
 def predict():
     request_data = request.get_json()
 
@@ -52,6 +52,39 @@ def predict():
         return('Prediction : Medium')
     else:
         return('Unknown')
+
+
+@app.route("/predictQuality", methods =['POST'])
+def predict_quality():
+    request_data = request.get_json()
+
+    model_path = 'ml-model/model_quality.h5'
+    loaded_model = load_model(model_path)
+
+    image_path = request_data['image']
+
+    file_name = 'gambar.jpg'
+
+    response = rq.get(image_path)
+    image_bytes = response.content
+
+    with open(file_name, 'wb') as file:
+        file.write(image_bytes)
+
+    gambar = Image.open(file_name)
+    resized_image = gambar.resize((224, 224))
+    x = image.img_to_array(resized_image)
+    x = np.expand_dims(x, axis=0)
+    x = x / 255.0  # Melakukan normalisasi gambar
+
+    # Melakukan prediksi menggunakan model
+    prediction = loaded_model.predict(x)
+    if prediction[0][0] > 0.8:
+        return('Prediction : Defect')
+    elif prediction[0][1] > 0.8:
+        return('Prediction : Good')
+    else:
+        return ('Unknown')
 
 
 if __name__ == '__main__':
